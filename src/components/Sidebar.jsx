965776b7d5c02817e8deef4418a027285fab00f7
Yaguera <ya-gotre@hotmail.com>
2024-01-11 20:15:25 -0300

    cores e estilos custom & add animations hover

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 
+import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs';
+import { FaFire, FaPoo } from 'react-icons/fa';
+
 function Sidebar() {
   return (
     <div className='fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg'>
-        <i>A</i>
-        <i>B</i>
-        <i>C</i>
-        <i>D</i>
-        <i>E</i>
+        <SidebarIcon icon={<FaFire size='28'/>}/>
+        <Divider />
+        <SidebarIcon icon={<BsPlus size="32" />} />
+        <SidebarIcon icon={<BsFillLightningFill size="20" />} />
+        <SidebarIcon icon={<FaPoo size="20" />} />
+        <Divider />
+        <SidebarIcon icon={<BsGearFill size="22" />} />
     </div>
   )
 }
+
+const SidebarIcon = ({icon}) => (
+    <div className='sidebar-icon'>
+        {icon}
+    </div>
+);
+
+const Divider = () => <hr className="sidebar-hr" />;
 
 export default Sidebar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 export default {
   mode:'jit',
   content: [
@@ -6,7 +8,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:'#202225',
+        secondary:'#5865f2',
+        gray: colors.trueGray,
+        gray:{
+          900:'#202225',
+          800:'#2f3136',
+          700:'#36393f',
+          600:'#4f545c',
+          400:'#d4d7dc',
+          300:'#e3e5e8',
+          200:'#ebedef',
+          100:'#f2f3f5',
+          // cores personalisadas
+        },
+      },
+    },
   },
   plugins: [],
 }
